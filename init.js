148b89a8446c5cde692527f165af08e730f5a33f
sanchuah <sanchuah@andrew.cmu.edu>
2015-12-19 10:04:58 -0500

    update js css

--- init.js
+++ init.js
@@ -12,14 +12,20 @@ function init() {
 // Basic options for a simple Google Map
 // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
 var mapOptions = {
+zoomControl: true,
+mapTypeControl: false,
+scaleControl: false,
+streetViewControl: false,
+rotateControl: false,
+
 // How zoomed in you want the map to start at (always required)
-zoom: 3,
+zoom: 4,
 
 // The latitude and longitude to center the map (always required)
 center: new google.maps.LatLng(avg_lat, avg_lng),
 
 // How you would like to style the map.
-styles: [{"featureType":"administrative.locality","elementType":"all","stylers":[{"hue":"#2c2e33"},{"saturation":7},{"lightness":19},{"visibility":"on"}]},{"featureType":"landscape","elementType":"all","stylers":[{"hue":"#ffffff"},{"saturation":-100},{"lightness":100},{"visibility":"simplified"}]},{"featureType":"poi","elementType":"all","stylers":[{"hue":"#ffffff"},{"saturation":-100},{"lightness":100},{"visibility":"off"}]},{"featureType":"road","elementType":"geometry","stylers":[{"hue":"#bbc0c4"},{"saturation":-93},{"lightness":31},{"visibility":"simplified"}]},{"featureType":"road","elementType":"labels","stylers":[{"hue":"#bbc0c4"},{"saturation":-93},{"lightness":31},{"visibility":"on"}]},{"featureType":"road.arterial","elementType":"labels","stylers":[{"hue":"#bbc0c4"},{"saturation":-93},{"lightness":-2},{"visibility":"simplified"}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"hue":"#e9ebed"},{"saturation":-90},{"lightness":-8},{"visibility":"simplified"}]},{"featureType":"transit","elementType":"all","stylers":[{"hue":"#e9ebed"},{"saturation":10},{"lightness":69},{"visibility":"on"}]},{"featureType":"water","elementType":"all","stylers":[{"hue":"#e9ebed"},{"saturation":-78},{"lightness":67},{"visibility":"simplified"}]}, {featureType: "administrative", elementType: "geometry", stylers: [{visibility:"off"}]}, {featureType: "administrative.country", elementType: "labels", stylers: [{visibility:"off"}]}, {featureType: "administrative.country", elementType: "geometry", stylers: [{visibility:"on"}]}]
+styles: [ {"featureType":"administrative", "elementType":"all", "stylers":[{"visibility":"off"}]}, {"featureType":"landscape", "elementType":"labels", "stylers":[{"visibility":"off"}]}, {"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#eeeeee"},{"visibility":"on"}, {"lightness":-8}]},{"featureType":"poi","elementType":"all","stylers":[{"hue":"#ffffff"},{"saturation":-100},{"lightness":100},{"visibility":"off"}]},{"featureType":"road","elementType":"geometry","stylers":[{"hue":"#bbc0c4"},{"saturation":-93},{"lightness":31},{"visibility":"simplified"}]},{"featureType":"road","elementType":"labels","stylers":[{"hue":"#bbc0c4"},{"saturation":-93},{"lightness":31},{"visibility":"on"}]},{"featureType":"road.arterial","elementType":"labels","stylers":[{"hue":"#bbc0c4"},{"saturation":-93},{"lightness":-2},{"visibility":"simplified"}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"hue":"#e9ebed"},{"saturation":-90},{"lightness":-8},{"visibility":"simplified"}]},{"featureType":"transit","elementType":"all","stylers":[{"hue":"#e9ebed"},{"saturation":10},{"lightness":69},{"visibility":"on"}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#ffffff"},{"saturation":-78},{"lightness":67},{"visibility":"simplified"}]},{featureType: "administrative.country", elementType: "labels", stylers: [{visibility:"off"}]}, {featureType: "administrative.country", elementType: "geometry", stylers: [{visibility:"on"}]}]
 };
 
 // Get the HTML DOM element that will contain your map
@@ -33,14 +39,17 @@ function gen_color(com) {
   var codes = "0123456789ABCDEF"
   var primes = [[7, 11], [11, 13], [17, 19], [23, 29], [31, 37], [41, 43]];
   var trans_code = primes.map(function(x){return codes[(com * x[0] + x[1])% 16 ]}).join("");
-  return "#"+trans_code;
+
+//  return "#"+trans_code;
+//  return "#BC1421";
+  return "#FF0000";
 }
 
 // ------------------------------------------------------------------------
 for (var a in airports) {
   var marker = new google.maps.Marker({
     position: airports[a].center, map: map,
-    icon: {path: google.maps.SymbolPath.CIRCLE,scale: 2, strokeOpacity: 1, strokeWeight: 1, fillOpacity: 0.75,
+    icon: {path: google.maps.SymbolPath.CIRCLE,scale: 2, strokeOpacity: 1, strokeWeight: 5, fillOpacity: 0.75,
       strokeColor: gen_color(airports[a].com), fillColor: gen_color(airports[a].com)
     }
   });
